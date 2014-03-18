7306e1f68081049d37ec349a0f7e5cc0c697e1e9
isma44 <isma44@gmail.com>
2014-03-19 01:03:19 +0900

    Time Setup by NTP

--- network_setup.ino
+++ network_setup.ino
@@ -1,3 +1,5 @@
+// Networking Setup for Galileo : Class B
+
 void setup() {
   Serial.begin(9600);
 
@@ -9,6 +11,9 @@ void setup() {
   
   // SSHアクセス出来たら、/etc/resolv.confも設定
   // nameserver 192.168.0.1
+  
+  // Time Setup by NTP (未来には出来ない可能性高い)
+  system("rdate -s time.bora.net");
   
   //system("cat /etc/issue > /dev/ttyGS0");
   //system("/etc/init.d/sshd stop > /dev/ttyGS0");
