07bef96d7e2c1622cc6d36871b7a6e403704191d
isma44 <isma44@gmail.com>
2014-03-30 16:12:21 +0900

    操作をJoystickスタイルで変更

--- clitest4.js
+++ clitest4.js
@@ -1,42 +1,37 @@
 
-// ______ Server Connection Setup ________
+var nDistMoved = 0;
 
 var io = require('socket.io-client');
 
+var hostMain = 'localhost';
 var portMain = 8080;
 var nsMain = 'pg';
-
-
-
-// ______ GPIO Setup for LED ________
- 
-var button_gpio = 14; // maps to digital PIN2 on the board. 14 for SoC, 32 for Cypr.
-
-
-// var led_gpio = 16;       // maps to digital PIN7 (NG)
-var led_right   = 17;       // maps to digital PIN5 (OK => RIGHT)
-var led_up      = 18;       // maps to digital PIN3 (OK => UP, but 15 was NG)
-var led_down    = 24;       // maps to digital PIN6 (OK => DOWN)
-// var led_gpio = 25;       // maps to digital PIN4 (NG)
-var led_left    = 26;       // maps to digital PIN8 (OK => LEFT)
-// var led_gpio = 27;       // maps to digital PIN7
-
-
-
-// _______ Common Valuables _______
 
 var $circle,
     playerId, browserName,
     nBallRadius = 40,
+    defaultWidth = 300, defaultHeight = 300,
     nFixX = 8, nFixY = 12,
-    xMax = 300 - nBallRadius*2,
-    yMax = 300 - nBallRadius*2,
+    xMax = defaultWidth - nBallRadius*2,
+    yMax = defaultHeight - nBallRadius*2,
     // nCntServerErr = 0,
     // nMaxCntErrForWarning = 2,
     xyForLED = {xDiff: 0, yDiff: 0},
-    nDistMoved = 0;
     socket;
     
+
+// __________ GPIO setup ___________
+
+var button_gpio = 14; // maps to digital PIN2 on the board. 14 for SoC, 32 for Cypr.
+// var button_gpio  = 17; // maps to digital PIN5 on the board
+
+// var led_gpio     = 16;    // maps to digital PIN7 (NG)
+var led_right       = 17;      // maps to digital PIN5 (OK => RIGHT)
+var led_up          = 18;         // maps to digital PIN3 (OK => UP, but 15 is NG)
+var led_down        = 24;       // maps to digital PIN6 (OK => DOWN)
+// var led_gpio     = 25;    // maps to digital PIN4 (NG)
+var led_left        = 26;       // maps to digital PIN8 (OK => LEFT)
+// var led_gpio     = 27; // maps to digital PIN7
 
 
 // _____________ connected/disconnected event of all client ______________
@@ -76,7 +71,7 @@ function drawNetworkOrServerProblem()
 
 function prepareConnectedAndDisconnectedEvent()
 {
-    socket = io.connect('http://hp2hp.net:'+portMain+'/'+nsMain);
+    socket = io.connect('http://'+hostMain+':'+portMain+'/'+nsMain);
     // socket = io.connect('http://'+location.hostname+':'+portMain+'/'+nsMain);
 
     socket.on('response', function( data )
@@ -155,74 +150,74 @@ var events = {
 
 
 
-    // var newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-    // $('.circle').css("background-color", newColor);
-    // $circle = $('.circle').show();
-    
-    // $("#distance").text('X, Yの差分 (複数の移動差分中、最終のみ)');
-    // console.log('X, Yの差分 (複数の移動差分中、最終のみ)');
+// var newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+// $('.circle').css("background-color", newColor);
+// $circle = $('.circle').show();
 
-
-    
-    prepareConnectedAndDisconnectedEvent();
+// $("#distance").text('X, Yの差分 (複数の移動差分中、最終のみ)');
+// console.log('X, Yの差分 (複数の移動差分中、最終のみ)');
 
 
 
-    
-    /********
-    $(window)
-        .on(events.start, function(e){
-            e.preventDefault();
+prepareConnectedAndDisconnectedEvent();
 
-            var xy = _getXY(e);
 
-            if ( $circle.css('display') === 'none' ) {
-                $circle.show();
-            }
-            $circle.css({top: xy.y, left: xy.x});
-            
-            $(this).on(events.move, _onMove);
-        })
-        .on(events.end, function(e){
-            e.preventDefault();
 
-            var xy = _getXY(e);
 
-            $circle.css({top: xy.y, left: xy.x});
-    
-            $(this).off(events.move, _onMove);
-        });
-        
-
-    var _onMove = function(e){
+/********
+$(window)
+    .on(events.start, function(e){
         e.preventDefault();
 
         var xy = _getXY(e);
+
+        if ( $circle.css('display') === 'none' ) {
+            $circle.show();
+        }
+        $circle.css({top: xy.y, left: xy.x});
         
-        socket.emit('move', {x: xy.x, y: xy.y });
-    };
-    *********/
+        $(this).on(events.move, _onMove);
+    })
+    .on(events.end, function(e){
+        e.preventDefault();
+
+        var xy = _getXY(e);
+
+        $circle.css({top: xy.y, left: xy.x});
+
+        $(this).off(events.move, _onMove);
+    });
     
-    var _getXY = function(/* MouseEvent */ e) {
-        var touches = e.originalEvent && e.originalEvent.touches ? e.originalEvent.touches : [],
-            x = nBallRadius, y = nBallRadius;
 
-        if ( _touchSupport && 0 < touches.length ) {
-            x = touches[0].pageX;
-            y = touches[0].pageY;
-        }
-        else {
-            x = e.pageX;
-            y = e.pageY;
-        }
+var _onMove = function(e){
+    e.preventDefault();
 
-        if ( x < 0 ) { x = nFixX; } 
-        if ( y < 0 ) { y = nFixY; } 
-        if ( x > xMax ) { x = xMax + nFixX; } 
-        if ( y > yMax ) { y = yMax + nFixY; } 
+    var xy = _getXY(e);
+    
+    socket.emit('move', {x: xy.x, y: xy.y });
+};
+*********/
 
-        return { x: x, y: y };
+var _getXY = function(/* MouseEvent */ e) {
+    var touches = e.originalEvent && e.originalEvent.touches ? e.originalEvent.touches : [],
+        x = nBallRadius, y = nBallRadius;
+
+    if ( _touchSupport && 0 < touches.length ) {
+        x = touches[0].pageX;
+        y = touches[0].pageY;
     }
+    else {
+        x = e.pageX;
+        y = e.pageY;
+    }
+
+    if ( x < 0 ) { x = nFixX; } 
+    if ( y < 0 ) { y = nFixY; } 
+    if ( x > xMax ) { x = xMax + nFixX; } 
+    if ( y > yMax ) { y = yMax + nFixY; } 
+
+    return { x: x, y: y };
+}
 
 
 
@@ -250,6 +245,7 @@ function getBrowserName()
 // ---------------------------------------
 
 var fs = require('fs');
+
  
 var fileOptions = {encoding: 'ascii'};
  
@@ -304,25 +300,25 @@ setGpioOut(led_down);
 // setGpioIn(button_gpio);
  
 // repeat reading/writing in a loop
-var nDelay = 10;
+var nDelay = 100;
 
-setInterval(function() {
+setInterval( function() {
 
     writeGpio( led_left, 0 );
     writeGpio( led_right, 0 );
     writeGpio( led_up, 0 );
     writeGpio( led_down, 0 );
 
-    if ( xyForLED.xDiff < 0 ) { 
+    if ( xyForLED.xDiff < -10 ) { 
         writeGpio( led_left, 1 );
     }
-    if ( xyForLED.xDiff > 0 ) { 
+    if ( xyForLED.xDiff > 10 ) { 
         writeGpio( led_right, 1 );
     }
-    if ( xyForLED.yDiff < 0 ) { 
+    if ( xyForLED.yDiff < -10 ) { 
         writeGpio( led_down, 1 );
     }
-    if ( xyForLED.yDiff > 0 ) { 
+    if ( xyForLED.yDiff > 10 ) { 
         writeGpio( led_up, 1 );
     }
   
